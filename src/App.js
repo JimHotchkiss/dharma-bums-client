8be0fb80a969ca430426e3690dab98e6c04cd6ad
github username <github email address>
2020-01-19 07:47:30 -0800

    Added createStore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 class App extends React.Component {
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/meditations/1/comments/1")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
   render() {
     return (
       <div>
