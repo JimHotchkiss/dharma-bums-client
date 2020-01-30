bac85fd2409fa39eca21c7f39639d00b088745e0
github username <github email address>
2020-01-30 15:37:46 -0800

    Created navbar-branch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 const NavBar = props => {
   return (
-    <div>
+    <div className="Nav-color">
       <Link to={`/meditations/new`}>Create Meditation</Link>
     </div>
   );
