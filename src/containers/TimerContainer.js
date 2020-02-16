bb0fdafbc2682e28bb7e843bf7616d3491502b89
github username <github email address>
2020-02-15 16:52:44 -0800

    Added audio to beginning and ending of meditaiton

--- src/containers/TimerContainer.js
+++ src/containers/TimerContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TimerContainer.css";
+import meditationChime from "../sounds/meditationBell.mp3";
 class TimerContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -7,37 +8,34 @@ class TimerContainer extends React.Component {
     this.state = { meditationLength: time };
   }
   render() {
+    const audio = new Audio(meditationChime);
+
     const handleStart = () => {
+      audio.play();
       const interval = setInterval(() => {
         this.setState({ meditationLength: this.state.meditationLength - 1 });
         if (this.state.meditationLength === 0) {
           clearInterval(interval);
+          audio.play();
         }
       }, 1000);
     };
     let timer = this.state.meditationLength;
     const complete = <p>Meditation Complete</p>;
-    const handlePause = () => {
-      // console.log("pause");
-    };
+
     const meditationComplete = <p>Mediation Complete</p>;
     return (
       <div className="Timer-container">
         <div className="Timer-title">
           <h2>Meditation Clock</h2>
-
           <div className="Timer-div">{timer === 0 ? complete : timer}</div>
         </div>
         <div className="Button-wrapper">
           <div className="Start-div">
             <button onClick={handleStart} className="Start-button">
-              <p>Start</p>
+              <p>Begin</p>
             </button>
-          </div>
-          <div className="Pause-div">
-            <button onClick={handlePause} className="Pause-button">
-              <p>Pause</p>
-            </button>
+            <audio className="Chime" src="../sounds/meditationBell.mp3"></audio>
           </div>
         </div>
       </div>
